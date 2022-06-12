d17fe3485780acdd81a5d4fed8cc5a09451367b7
kesel <a.kiselyoff@gmail.com>
2022-06-12 20:14:42 +0300

    add loader

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import { TailSpin } from 'react-loader-spinner';
 import ImageGallery from './ImageGallery';
 
 const KEY = '26823171-490067530a76e346906bfc05d';
@@ -7,13 +9,16 @@ export default class App extends Component {
   state = {
     gallery: [],
     page: 1,
+    loading: false,
   };
   componentDidMount() {
+    this.setState({ loading: true });
     fetch(
       `https://pixabay.com/api/?q=cat&page=1&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
     )
       .then(res => res.json())
-      .then(gallery => this.setState({ gallery: gallery.hits }));
+      .then(gallery => this.setState({ gallery: gallery.hits }))
+      .finally(() => this.setState({ loading: false }));
   }
   componentDidUpdate() {
     console.log(this.state.gallery);
@@ -21,6 +26,9 @@ export default class App extends Component {
   render() {
     return (
       <>
+        {this.state.loading && (
+          <TailSpin color="#00BFFF" height={80} width={80} />
+        )}
         <ImageGallery gallery={this.state.gallery} />
       </>
     );
