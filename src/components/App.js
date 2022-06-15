87434ea444cd6e1688a684d705edc45dca7e3dfe
kesel <a.kiselyoff@gmail.com>
2022-06-15 14:35:23 +0300

    replace from Gallery to App comp

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,101 @@
 import { Component } from 'react';
-
+import { TailSpin } from 'react-loader-spinner';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import Searchbar from './Searchbar/Searchbar';
+import fetchGallery from './fetchAPI';
 import ImageGallery from './ImageGallery/ImageGallery';
+import ImageGalleryError from './ImageGallery/ImageGalleryError';
+import Button from './Button/Button';
+import Modal from './Modal/Modal';
 
 export default class App extends Component {
   state = {
+    gallery: [],
     query: '',
+    page: 1,
+    error: null,
+    status: 'idle',
+    isModalOpen: false,
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    const prevQuery = prevProps.query;
+    const nextQuery = this.props.query;
+    const prevPage = prevState.page;
+    const nextPage = this.state.page;
+
+    if (prevPage !== nextPage || prevQuery === nextQuery) {
+      this.setState({ status: 'pending' });
+
+      fetchGallery
+        .fetchAPI(nextQuery, nextPage)
+        .then(gallery => {
+          this.setState({
+            gallery: [...prevState.gallery, ...gallery.hits],
+            status: 'resolved',
+          });
+        })
+        .catch(error => this.setState({ error, status: 'rejected' }));
+    }
+  }
 
   handleFormSubmit = query => {
     this.setState({ query });
   };
 
-  render() {
-    const { query } = this.state;
-    return (
-      <div>
-        <Searchbar onSubmit={this.handleFormSubmit} />
+  handleLoadMore = () => {
+    this.setState(({ page }) => {
+      return { page: page + 1 };
+    });
+  };
 
-        <ImageGallery query={query} />
-      </div>
+  onImageClick = evt => {
+    if (evt.target.nodeName === 'IMG') {
+      const imgId = Number(evt.target.attributes.id.value);
+
+      this.state.gallery.forEach(item => {
+        const itemId = Number(item.id);
+
+        if (itemId === imgId) {
+          this.setState({
+            dataModalImg: item,
+            isModalOpen: true,
+          });
+        }
+      });
+    }
+  };
+
+  closeModal = () => {
+    this.setState({
+      isModalOpen: false,
+    });
+  };
+
+  render() {
+    const { gallery, query, error, status, isModalOpen } = this.state;
+    return (
+      <>
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        {status === 'idle' && <h1>Enter your query</h1>}
+        {status === 'pending' && (
+          <TailSpin
+            ariaLabel="loading"
+            color="#00BFFF"
+            height={80}
+            width={80}
+          />
+        )}
+        {status === 'resolved' && <ImageGallery query={query} />}
+        {status === 'rejected' && <ImageGalleryError message={error.message} />}
+        {gallery.length > 0 && <Button onClick={this.handleLoadMore} />}
+        {isModalOpen && (
+          <Modal
+            imgModal={this.state.dataModalImg}
+            closeModal={this.closeModal}
+          />
+        )}
+      </>
     );
   }
 }
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,135 +1,29 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
-import { TailSpin } from 'react-loader-spinner';
-import fetchGallery from '../fetchAPI';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
-import ImageGalleryError from './ImageGalleryError';
-import Button from '../Button/Button';
-import Modal from '../Modal/Modal';
 import s from './ImageGallery.module.css';
 
 class ImageGallery extends Component {
   state = {
     gallery: [],
-    query: '',
-    page: 1,
-    error: null,
-    status: 'idle',
-    isModalOpen: false,
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    const prevQuery = prevProps.query;
-    const nextQuery = this.props.query;
-    const prevPage = prevState.page;
-    const nextPage = this.state.page;
-
-    if (prevQuery !== nextQuery) {
-      this.setState({ status: 'pending', page: 1 });
-
-      fetchGallery
-        .fetchAPI(nextQuery, nextPage)
-        .then(gallery => {
-          this.setState({
-            gallery: gallery.hits,
-            status: 'resolved',
-          });
-        })
-        .catch(error => this.setState({ error, status: 'rejected' }));
-    }
-
-    if (prevPage !== nextPage && prevQuery === nextQuery) {
-      this.setState({ status: 'pending' });
-
-      fetchGallery
-        .fetchAPI(nextQuery, nextPage)
-        .then(gallery => {
-          this.setState({
-            gallery: [...prevState.gallery, ...gallery.hits],
-            status: 'resolved',
-          });
-        })
-        .catch(error => this.setState({ error, status: 'rejected' }));
-    }
-  }
-
-  handleLoadMore = () => {
-    this.setState(({ page }) => {
-      return { page: page + 1 };
-    });
-  };
-
-  onImageClick = evt => {
-    if (evt.target.nodeName === 'IMG') {
-      const imgId = Number(evt.target.attributes.id.value);
-
-      this.state.gallery.forEach(item => {
-        const itemId = Number(item.id);
-
-        if (itemId === imgId) {
-          this.setState({
-            dataModalImg: item,
-            isModalOpen: true,
-          });
-        }
-      });
-    }
-  };
-
-  closeModal = () => {
-    this.setState({
-      isModalOpen: false,
-    });
   };
 
   render() {
-    const { gallery, error, status, isModalOpen } = this.state;
+    const { gallery } = this.state;
 
-    // if (status === 'idle') {
-    //   return <h1>Enter your query</h1>;
-    // }
-    if (status === 'pending') {
-      return (
-        <div className={s.TailSpin}>
-          <TailSpin
-            ariaLabel="loading"
-            color="#00BFFF"
-            height={80}
-            width={80}
-          />
-        </div>
-      );
-    }
-    if (status === 'rejected') {
-      return <ImageGalleryError message={error.message} />;
-    }
-
-    if (status === 'resolved') {
-      return (
-        <>
-          {isModalOpen && (
-            <Modal
-              imgModal={this.state.dataModalImg}
-              closeModal={this.closeModal}
+    return (
+      <ul className={s.ImageGallery}>
+        {gallery.map(galleryItem => {
+          return (
+            <ImageGalleryItem
+              onImageClick={this.onImageClick}
+              key={galleryItem.id}
+              galleryItem={galleryItem}
             />
-          )}
-          <ul className={s.ImageGallery}>
-            {gallery.map(galleryItem => {
-              return (
-                <ImageGalleryItem
-                  onImageClick={this.onImageClick}
-                  key={galleryItem.id}
-                  galleryItem={galleryItem}
-                />
-              );
-            })}
-          </ul>
-
-          {gallery.length > 0 && <Button onClick={this.handleLoadMore} />}
-        </>
-      );
-    }
+          );
+        })}
+      </ul>
+    );
   }
 }
 
